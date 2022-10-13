2f24960bb15eb9f764a1e7713997392aa1968d9a
3faze <93428357+3faze@users.noreply.github.com>
2022-10-13 20:46:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import time
 
-file = open("file.mer", "r+")                                         file_read = file.read()
-conts = file_read.split("\n")                                         
+file = open("file.mer", "r+")                                         
+file_read = file.read()
+conts = file_read.split("\n")  
+
+class Var:
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+    def assign_var(self):
+        return self.name, self.value 
+
 for i in conts:
     if i != "" or i != " ":
-        if "PRINT " in i:                                                         #print("Token found:", "PRINT")
+        if "PRINT " in i:                                                         
+            #print("Token found:", "PRINT")
             try:
                 stri = i.split("PRINT")
                 fin = stri[1].split('"')[1]
@@ -16,7 +26,8 @@ for i in conts:
                     print(fin)
                 except:
                     print("Couldn't print the number.")
-        elif "WAIT" in i:                                                         try:
+        elif "WAIT" in i:                                                        
+            try:
                 stri = i.split("WAIT")
                 fin = stri[1]
                 time.sleep(float(fin))
@@ -31,3 +42,12 @@ for i in conts:
                 print(eval(fin2[0]))
             except:
                 print("Couldn't execute the expression")
+        elif "VAR" in i:
+                stri = i.split("VAR")
+                fin1 = stri[1].split(" ")
+                
+                var_name = fin1[1]
+                var_value = fin[3]
+
+                vari = Var(var_name, var_value)
+                print(vari.assign_var())
