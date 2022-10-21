e593d5c545648c0a75a85acd8b3c70f1e2bbe4ee
3faze <93428357+3faze@users.noreply.github.com>
2022-10-21 15:16:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,83 @@
 import time
+import random
 
 time.sleep(1)
+
+# -----------------------------------------------------
+conts_e = None
+
+def exec(conts_e):
+  if conts_e != "" or conts_e != " " or "//" not in conts_e:
+    if "PRINT " in conts_e:
+      try:
+        stri = conts_e.split("PRINT")
+        if '"' in stri[1]:
+          fin = stri[1].split('"')
+          print(fin[1])
+        for x in types:
+          if x in stri[1]:
+            print(x)
+        for j in var_names:
+          if j in stri[1]:
+            print(vars[j])
+        for z in inps:
+          if z in stri[1]:
+            print(inps[z])
+      except:
+        try:
+          stri = conts_e.split("PRINT")
+          fin = stri[1]
+          print(fin)
+        except:
+          print("Couldn't print the number.")
+    elif "WAIT" in conts_e:
+      try:
+        stri = conts_e.split("WAIT")
+        fin = stri[1]
+        time.sleep(float(fin))
+      except:
+        print("Couldn't delay the program.")
+    elif "EVAL" in conts_e:
+      try:
+        stri = conts_e.split("EVAL")
+        fin1 = stri[1].split("(")
+        fin2 = fin1[1].split(")")
+        print(eval(fin2[0]))
+      except:
+        print("Couldn't execute the expression")
+    elif "VAR" in conts_e:
+      try:
+        stri = conts_e.split("VAR")
+        fin1 = stri[1].split(" ")
+        var_name = fin1[1]
+        var_names.append(var_name)
+        if '"' in fin1[3] and "VAR" in conts_e:
+          fin2 = fin1[3].split('"')
+          var_value = fin2[1]
+        elif "Null" in conts_e and "VAR" in conts_e:
+          var_value = fin1[3]
+        else:
+          if "VAR" in conts_e:
+            var_value = fin1[3]
+          else:
+            print("Wrong data type")
+        vars[var_name] = var_value
+      except:
+        print("Couldn't define variable.")
+    elif "INPUT" in conts_e:
+      try:
+        stri_inp = conts_e.split("INPUT")
+        inp_fin1 = stri_inp[1].split("=>")
+        txt_get = stri_inp[1].split('"')[1]
+        a = input(txt_get)
+        txt_var = inp_fin1[1].split(" ")[1]
+        inps[txt_var] = a
+      except:
+        print("Couldn't do an input.")
+    elif "QUIT" in conts_e:
+      quit()
+
+# -----------------------------------------------------
 
 vars = {}
 inps = {}
@@ -15,7 +92,7 @@ file_read = file.read()
 conts = file_read.split("\n")
 for i in conts:
   if i != "" or i != " " or "//" not in i:
-    if "PRINT " in i:
+    if "PRINT " in i and not "FOR" in i:
       try:
         stri = i.split("PRINT")
         if '"' in stri[1]:
@@ -38,7 +115,7 @@ for i in conts:
           print(fin)
         except:
           print("Couldn't print the number.")
-    elif "WAIT" in i:
+    elif "WAIT" in i and not "FOR" in i:
       try:
         stri = i.split("WAIT")
         fin = stri[1]
@@ -46,7 +123,7 @@ for i in conts:
       except:
         print("Couldn't delay the program.")
 
-    elif "EVAL" in i:
+    elif "EVAL" in i and not "FOR" in i:
       try:
         stri = i.split("EVAL")
         fin1 = stri[1].split("(")
@@ -55,7 +132,7 @@ for i in conts:
       except:
         print("Couldn't execute the expression")
 
-    elif "VAR" in i:
+    elif "VAR" in i and not "FOR" in i:
       try:
         stri = i.split("VAR")
         fin1 = stri[1].split(" ")
@@ -75,7 +152,7 @@ for i in conts:
       except:
         print("Couldn't define variable.")
 
-    elif "INPUT" in i:
+    elif "INPUT" in i and not "FOR" in i:
       try:
         stri_inp = i.split("INPUT")
         inp_fin1 = stri_inp[1].split("=>")
@@ -85,6 +162,15 @@ for i in conts:
         inps[txt_var] = a
       except:
         print("Couldn't do an input.")
+
+    elif "FOR" in i:
+      stri = i.split("FOR")
+      fin1 = stri[1].split(" ")
+      brack1 = stri[1].split("(")
+      brack2 = brack1[1].split(")")
+      #Brack2[0] is the index to the execution
+      for f_loop in range(int(fin1[3])):
+        exec(brack2[0])
 
     elif "QUIT" in i:
       quit()
